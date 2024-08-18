981418cf3450bc17d43a4aa4f304b44dbb591064
laurascotelari <laura.scotelari@usp.br>
2024-08-18 17:17:37 -0300

    Criando projeto

---
+++ nike-project/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+
+export{
+    Hero,
+    PopularProducts,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    Footer,
+    CustomerReviews,
+    Services
+};
